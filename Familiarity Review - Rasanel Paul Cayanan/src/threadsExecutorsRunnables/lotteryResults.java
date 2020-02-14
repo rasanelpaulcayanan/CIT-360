d86cafdf7d7239f6245ed72ec57c9837da973ec0
cay18002@byui.edu <cay18002@byui.edu>
2020-02-14 10:26:22 +0900

    Hello

--- Familiarity Review - Rasanel Paul Cayanan/src/threadsExecutorsRunnables/lotteryResults.java
+++ Familiarity Review - Rasanel Paul Cayanan/src/threadsExecutorsRunnables/lotteryResults.java
@@ -16,8 +16,10 @@ package threadsExecutorsRunnables;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Random;
 
 public class lotteryResults {
 
@@ -41,6 +43,7 @@ public class lotteryResults {
 	System.out.print("Press 3 for Maine 3 numbers!\n");
 	System.out.print("Press 4 for All States\n");
 	System.out.print("Press 5 to exit\n");
+	System.out.print("Press 6 to generate random lottery numbers\n");
 	System.out.print("\nWARNING! You can only select from the options above: ");
 	
 	//Switch utilized to provide options for end user.
@@ -69,6 +72,50 @@ public class lotteryResults {
     	System.out.print("Thank you for trying!\n");
     	break;
     	
+    case 6:
+    	ExecutorService executor = Executors.newFixedThreadPool(10);
+    	
+    	for (int i = 0; i < 10 ; i++ ) {
+    		
+    	Runnable thread = new Runnable() {
+
+			@Override
+			public void run() {
+				Random randomize = new Random();
+				
+				int number1 = randomize.nextInt(100); 
+				int number2 = randomize.nextInt(100); 
+				int number3 = randomize.nextInt(100); 
+				int number4 = randomize.nextInt(100); 
+				int number5 = randomize.nextInt(100); 
+			
+			
+			System.out.println("Lottery Results" + number1 + " " + number2 + " " + number3 + " " + number4 + " " + number5); 
+				
+			}  		
+    	};	
+    		
+    	executor.execute(thread); 
+    	
+    	}
+    	
+    	try {
+    	    System.out.println("Loading Results.. ");
+    	    executor.shutdown();
+    	    executor.awaitTermination(5, TimeUnit.SECONDS);
+    	}
+    	catch (InterruptedException e) {
+    	    System.err.println("tasks interrupted");
+    	}
+    	finally {
+    	    if (!executor.isTerminated()) {
+    	        System.err.println("cancel non-finished tasks");
+    	    }
+    	    executor.shutdownNow();
+    	    System.out.println("You are done with the test Lottery results");
+    	}
+    	
+    	break;
     default:
     	System.out.print("You did not select from the choices above!\n");
     	continue;
